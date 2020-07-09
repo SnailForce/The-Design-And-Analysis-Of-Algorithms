a2f5b79cef7fbab1ff22a01d3e94e9426e575746
CoolFire <1078374406@qq.com>
2020-07-09 10:24:37 +0800

    Complete All

--- Assessment/Project3/Project3/Source.cpp
+++ Assessment/Project3/Project3/Source.cpp
@@ -7,8 +7,6 @@ using namespace std;
 constexpr auto N = 10;
 constexpr auto INF = 10000;
 
-// vector<int> path;
-
 class TSP
 {
 private:
@@ -16,7 +14,6 @@ private:
 	size_t r;
 	vector<vector<int>> distance;
 	vector<vector<int>> dp;
-	vector<vector<int>> path;
 public:
 	TSP();
 	void getShortestDistance();
@@ -25,9 +22,10 @@ public:
 
 TSP::TSP()
 {
-	cout << "input citys' distance " << endl;
+	cout << "input citys' num " << endl;
 	cin >> numOfCity;
-	r = 1 << (numOfCity - 1);
+	r = 1 << (numOfCity - 1);  // dp矩阵每一行的维度
+	cout << "input citys' distance " << endl;
 	/*
 	10000 8 5 36
 	6 10000 9 5
@@ -48,7 +46,7 @@ TSP::TSP()
 	for (size_t i = 0; i < numOfCity; i++)
 	{
 		vector<int> tmp;
-		tmp.assign(r, INF);
+		tmp.assign(r, INF);  // 初始化为最大值
 		dp.push_back(tmp);
 	}
 }
@@ -57,20 +55,23 @@ void TSP::getShortestDistance()
 {
 	for (size_t i = 0; i < numOfCity; i++)
 	{
-		dp.at(i).at(0) = distance.at(i).at(0);
+		dp.at(i).at(0) = distance.at(i).at(0);  // 第一列相当于从源点直接到达目的点
 	}
 	for (size_t j = 1; j < r; j++)
 	{
 		for (size_t i = 0; i < numOfCity; i++)
 		{
+			// 判断该组合是否包含源点
 			if (i > 0 && ((j >> (i - 1)) & 1) == 1)
 			{
 				continue;
 			}
 			for (size_t k = 1; k < numOfCity; k++)
 			{
+				// 判断k城市是否在当前组合j中
 				if ((j >> (k - 1) & 1) == 1)
 				{
+					// dp方程
 					if (dp.at(i).at(j) > distance.at(i).at(k) + dp.at(k).at(j ^ (1 << (k - 1))))
 					{
 						dp.at(i).at(j) = distance.at(i).at(k) + dp.at(k).at(j ^ (1 << (k - 1)));
@@ -80,7 +81,7 @@ void TSP::getShortestDistance()
 		}
 	}
 }
-
+// 打印dp数组
 void TSP::printDP()
 {
 	for (size_t i = 0; i < numOfCity; i++)
@@ -91,6 +92,7 @@ void TSP::printDP()
 		}
 		cout << endl;
 	}
+	// 第一行最后一个刚好是结果
 	cout << "The length of the shortest path is " << dp.at(0).back() << endl;
 }
 
